35b2dc63b166d37b6b6139640b598528465b8eb1
jonas brito <jonas2004brito@gmail.com>
2023-01-23 22:26:37 -0300

    #0000003 - melhoria de codigo

--- src/app/index.js
+++ src/app/index.js
@@ -1,26 +1,103 @@
-let body = document.body;
-let AllElementsBody = document.querySelector('body').children;
+// let body = document.body;
+let allElementsPage = [];
+allElementsPage.push(document.querySelectorAll('*'));
 
-if ( body.id == null || body.id == undefined || body.id === ' ') {
+let elementStyle = document.createElement('style');
 
-  body.id = randomNumber();
-}
+elementStyle.id = randomNumber();
+//manipular pseudo-elementos via javascript
 
-for ( let AllElements of AllElementsBody ) {
+// if ( body.id == null && body.id == undefined && body.id === ' ') {
 
-  if ( AllElements.localName != 'script' ) {
+//   body.id = randomNumber();
+// }
 
-    if ( AllElements.id == null || AllElements.id == undefined || AllElements.id === '' ) {
+allElementsPage.forEach((nodeElementsList) => {
+  
+nodeElementsList.forEach((nodeElement) => {
+  
+  
+  let i = 0;
+  while (i < nodeElement.children.length) {
 
-      AllElements.id = randomNumber();
-    }
-
+      if ( 
+      nodeElement.children.item(i).localName !== 'html' && nodeElement.children.item(i).localName !== 'head' && 
+      nodeElement.children.item(i).localName !== 'meta' && nodeElement.children.item(i).localName !== 'base' && 
+      nodeElement.children.item(i).localName !== 'title' && nodeElement.children.item(i).localName !== 'link' && 
+      nodeElement.children.item(i).localName !== 'script' ) 
+      {
+        let elementBody = document.querySelector('body')
+        elementBody.appendChild(elementStyle)
+  console.log(elementBody)
+        // console.log(nodeElement.children.item(i).style = '::after')
+        // console.log(nodeElement.children.item(i).style.border = '1px solid blue')
+        // // console.log(nodeElement.children.item(i).style.listStyle = "none")
+        // console.log(nodeElement.children.item(i).className)
+      }
+    
+    i++
   }
   
- 
-  console.log(AllElements)
-}
 
+})
+
+});
+// allElementsBody.forEach((elements) => {
+
+//   let i = 0;
+//   let parentElements = [];
+//   while ( i < elements.length ) {
+
+//     parentElements = elements.item( i ).children
+//     if ( elements.item( i ).localnodeElement.children.item(i).localName !== 'script' &&&  elements.item( i ).localnodeElement.children.item(i).localName !== 'head') {
+      
+     
+      
+//       console.log(elements.item( i ).children)
+     
+//     }
+
+//     i++
+//   }
+
+// });
+
+
+// for ( let i = 0; i < allElementsBody.length; i++) {
+
+
+  // if ( parentElements.localnodeElement.children.item(i).localName !== 'script' ) {
+
+    // if (parentElements.children.length !== 0 &&& parentElements.id !== null && parentElements.id !== '') {
+      
+      // for(let elements of parentElements) {
+      //   let childsElements = document.querySelectorAll('#'+elements.id+' > *')
+      //   console.log(childsElements)
+
+      // }
+     
+      // for(let child of parentElements.children) {
+      //   childsElements.push(document.querySelectorAll(child.localnodeElement.children.item(i).localName));
+      //   let childs = document.querySelectorAll(`${parentElements.localnodeElement.children.item(i).localName} > *`) 
+      // }
+    // }
+
+    // for (let i = 0; parentElements.children.length !== 0, i < parentElements.children.length; i++) {
+    //   let childsElements = parentElements.children[i];
+    //   console.log('aqui->',parentElements.children[i])
+    // }
+    
+   
+  //   if ( parentElements.id == null && parentElements.id == undefined && parentElements.id === '' ) {
+
+  //     parentElements.id = randomNumber();
+  //   }
+
+  // }
+  
+ 
+  // console.log(allElementsBody)
+// }
 
 
 function randomNumber() {
@@ -29,21 +106,21 @@ function randomNumber() {
   return Math.floor( Math.floor( Math.random() * Date.now() ) * Math.random() ).toString( 36 );
 }
 
-// if ( bodyId != null && bodyId == document.body.id ) {
+// if ( bodyId !== null &&& bodyId == document.body.id ) {
 
 //   let body = document.body;
-//   let AllElementsOfPage = document.documentElement.querySelectorAll( '*' );
+//   let allElementsOfPage = document.documentElement.querySelectorAll( '*' );
 
 //   body.innerHTML = ` ${ body.innerHTML } <h1>ola</h1> `;
 
-//   for ( let AllElements of AllElementsOfPage ) {
+//   for ( let allElements of allElementsOfPage ) {
     
-//     if ( AllElements.id != null && AllElements.id === bodyId) {
-//       console.log(AllElements.id);
+//     if ( parentElements.id !== null &&& parentElements.id === bodyId) {
+//       console.log(parentElements.id);
 //     }
     
 //   }
 
-//   console.log( AllElementsOfPage );
+//   console.log( allElementsOfPage );
 
 // }
